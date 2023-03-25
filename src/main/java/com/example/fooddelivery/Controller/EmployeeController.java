d225c29d17ea38edfde6d64317938cdf213ebfbf
leting sun <leting@wustl.edu>
2023-03-24 21:50:04 -0500

    day1 done

--- src/main/java/com/example/fooddelivery/Controller/EmployeeController.java
+++ src/main/java/com/example/fooddelivery/Controller/EmployeeController.java
@@ -1,10 +1,12 @@
 package com.example.fooddelivery.Controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+//import com.example.mybatisplustest.common.R;
+//import com.example.mybatisplustest.entity.Employee;
+//import com.example.mybatisplustest.service.EmployeeService;
 import com.example.fooddelivery.Service.EmployeeService;
 import com.example.fooddelivery.common.R;
 import com.example.fooddelivery.entity.Employee;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
@@ -13,23 +15,24 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
 @RestController
-@RequestMapping("/employee")//提前设置前缀，那么http://localhost:8080/employee/login就只要处理login就可以了
+@RequestMapping("/employee")
 public class EmployeeController {
+
     @Autowired
     private EmployeeService employeeService;
 
     /**
-     * 员工登陆
+     * 员工登录
      * @param request
      * @param employee
      * @return
      */
     @PostMapping("/login")
-    //如何接受json数据,注意表格的名称需要和实体类的属性名一样
-    //request 是用来存入session 保持登陆的
-    public R<Employee> login(HttpServletRequest request,@RequestBody Employee employee){
+    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee){
 
         //1、将页面提交的密码password进行md5加密处理
         String password = employee.getPassword();
@@ -59,29 +62,16 @@ public class EmployeeController {
         request.getSession().setAttribute("employee",emp.getId());
         return R.success(emp);
     }
-//    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee){
-//        //1. 将密码进行加密
-//        String password = employee.getPassword();
-//        password = DigestUtils.md5DigestAsHex(password.getBytes());
-//        //2. 根据用户名查询数据库，无用户名返回false
-//        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
-//        queryWrapper.eq(Employee::getUsername,employee.getUsername());
-//        Employee employee1 = employeeService.getOne(queryWrapper);
-//
-//        if(employee1 == null){
-//            return R.error("登陆失败");
-//        }
-//        //3. 将密码比对
-//        if(employee1.getPassword() != password){
-//            return R.error("密码不正确");
-//        }
-//        //4. 员工有禁用状态，查看员工是否禁用
-//        if(employee1.getStatus() == 0){
-//            return R.error("员工被禁用");
-//        }
-//        //5. 登陆成功，将员工存入session
-//        request.getSession().setAttribute("employee",employee1.getId());
-//        return R.success(employee1);
-//    }
 
+    /**
+     * 员工退出
+     * @param request
+     * @return
+     */
+    @PostMapping("/logout")
+    public R<String> logout(HttpServletRequest request){
+        //清理Session中保存的当前登录员工的id
+        request.getSession().removeAttribute("employee");
+        return R.success("退出成功");
+    }
 }
